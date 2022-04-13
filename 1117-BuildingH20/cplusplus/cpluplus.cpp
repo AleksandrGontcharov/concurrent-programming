25fddd73eedec5d1a88eca1d4a339c288093ab4e
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-04-13 13:05:41 -0700

    Complete CSharp soln

---
+++ 1117-BuildingH20/cplusplus/cpluplus.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+using namespace std;
+
+std::mutex gLock;
+std::condition_variable gConditionVariable;
+
+class ZeroEvenOdd {
+private:
+    int n;
+    int state = 1;
+
+public:
+    ZeroEvenOdd(int n) {
+        this->n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(function<void(int)> printNumber) {
+
+            for (int i = 1; i <= n; i++) {
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    gConditionVariable.wait(lock, [&](){ return state == 1;});
+                }
+
+                printNumber(0);
+
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    if (i % 2 == 1) {
+                        state = 3;
+                    } else {
+                        state = 2;
+                    }
+                }
+                gConditionVariable.notify_all();
+            }
+        }
+
+        void even(function<void(int)> printNumber) {
+             for (int i = 2; i <= n; i = i + 2) {
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    gConditionVariable.wait(lock, [&](){ return state == 2;});
+                }
+                
+                printNumber(i);
+                
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    state = 1;
+                }
+                gConditionVariable.notify_all();
+            }
+        }
+        void odd(function<void(int)> printNumber) {
+             for (int i = 1; i <= n; i = i + 2) {
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    gConditionVariable.wait(lock, [&](){ return state == 3;});
+                }
+                
+                printNumber(i);
+
+                {
+                    std::unique_lock<std::mutex> lock(gLock);
+                    state = 1;
+                }
+                gConditionVariable.notify_all();
+            }
+        }
+};
+
+void printNumber(int x) {
+    std::cout << x;
+}
+
+int main()
+{
+    ZeroEvenOdd foo(8);
+
+
+    std::thread threadA([&] {
+                foo.zero(printNumber);
+                });
+    
+    std::thread threadB([&] {
+        foo.even(printNumber);
+        });
+    
+    std::thread threadC([&] {
+        foo.odd(printNumber);
+        });
+    
+
+    threadA.join();
+    threadB.join();
+    threadC.join();
+
+    return(0);
+}
---
+++ 1117-BuildingH20/csharp/Program.cs
@@ -0,0 +1,87 @@
+﻿// https://leetcode.com/problems/print-in-order/
+
+namespace program;
+public class H20 {
+    
+    
+    public H20() {
+    }
+
+    List<string> _state = new List<string>() { "H", "H", "O" };
+    
+    private readonly object _locker = new object();
+
+    public void Hydrogen(Action releaseHydrogen) {    
+        lock (_locker)
+        {
+            while (!(_state.Contains("H")))
+                {
+                Monitor.Wait(_locker);
+            }
+         
+
+            releaseHydrogen();
+
+       
+            _state.Remove("H");
+            if (_state.Count == 0) {
+                _state.Add("H");
+                _state.Add("H");
+                _state.Add("O");
+            }
+            Monitor.PulseAll(_locker); 
+        }
+    }
+    
+     public void Oxygen(Action releaseOxygen) {
+
+        lock (_locker)
+        {
+            while (!(_state.Contains("O")))
+                {
+                Monitor.Wait(_locker);
+            }
+        
+        
+            releaseOxygen();
+
+        
+            _state.Remove("O");
+            if (_state.Count == 0) {
+                _state.Add("H");
+                _state.Add("H");
+                _state.Add("O");
+            }
+            Monitor.PulseAll(_locker); 
+        }
+    }
+
+
+    static void Main()
+    {
+        H20 foo = new H20();
+
+        // Create threads and assign names
+
+        void releaseHydrogen() {
+            Console.Write("H");
+        }
+        void releaseOxygen() {
+            Console.Write("O");
+        }
+
+        Thread threadH_1 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH_2 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH_3 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH_4 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadO_1 = new Thread(() => foo.Oxygen(releaseOxygen));
+        Thread threadO_2 = new Thread(() => foo.Oxygen(releaseOxygen));
+
+        threadH_1.Start();
+        threadH_2.Start();
+        threadH_3.Start();
+        threadH_4.Start();
+        threadO_1.Start();
+        threadO_2.Start();
+    }
+};
