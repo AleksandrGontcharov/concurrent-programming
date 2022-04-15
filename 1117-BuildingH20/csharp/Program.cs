af377da35a5a7df0525a8cc1188e83eb8d4343e2
Aleksandr Gontcharov <agontcharov@microsoft.com>
2022-04-15 01:02:40 -0700

    Second attempt

--- 1117-BuildingH20/csharp/Program.cs
+++ 1117-BuildingH20/csharp/Program.cs
@@ -1,65 +1,53 @@
-﻿// Problem Description:
-// Given a class 
-// public class Foo {
-//   public void first() { print("first"); }
-//   public void second() { print("second"); }
-// }
-
-// The same instance of Foo will be passed to an even number of threads. Half of the threads
-// will call the first method, while the other half will call the second method.
-// Modify the program such that the output is "firstsecondfirstsecond...".
-
-// So for example, if the number of threads is 4, the result should be "firstsecondfirstsecond"
-// So for example, if the number of threads is 6, the result should be "firstsecondfirstsecondfirstsecond"
-// So for example, if the number of threads is 8, the result should be "firstsecondfirstsecondfirstsecondfirstsecond"
-
-// and so on
+﻿
 
 
 using System.Globalization;
 using System.Runtime.ExceptionServices;
 
 namespace FirstSecond;
-public class H2O
-{
+using System.Threading;
+public class H2O {
 
-    public H2O()
-    {
-
+    public H2O() {
+        
     }
-
+    
     static readonly object _locker = new object();
 
-    int _state = 1;
-    bool _hydrogenwasprinted = false;
-    bool _oxygenwasprinted = true;
+    int _switch = 1;
+    
+    int _number_of_H_printed = 0;
+
+    int _number_of_O_printed = 0;
 
 
     public void Hydrogen(Action releaseHydrogen)
     {
         lock (_locker)
         {
-            while (!(_state == 1 && _oxygenwasprinted))
+            while (!(_switch == 1 && _number_of_H_printed < 2))
             {
-                Console.WriteLine($"    {Thread.CurrentThread.Name} acquiring lock");
                 Monitor.Wait(_locker);
-                Console.WriteLine($"    {Thread.CurrentThread.Name} releasing lock");
-                if (_hydrogenwasprinted)
+                if (_number_of_O_printed < 1 && _number_of_H_printed == 2)
                 {
-                    Console.WriteLine($"        {Thread.CurrentThread.Name} pulsing one before reacquiring lock");
                     Monitor.Pulse(_locker);
                 }
             }
-            _state = 2;
+            _switch = 2;
         }
 
         releaseHydrogen();
-
         lock (_locker)
         {
-            _hydrogenwasprinted = true;
-            _oxygenwasprinted = false;
-            Console.WriteLine($"        {Thread.CurrentThread.Name} pulsing one at the end");
+            _number_of_H_printed += 1;
+            _switch = 1;
+
+            if (_number_of_H_printed == 2 && _number_of_O_printed == 1)
+            {
+                _number_of_H_printed = 0;
+                _number_of_O_printed = 0;
+            }
+
             Monitor.Pulse(_locker);
         }
     }
@@ -68,26 +56,33 @@ public class H2O
     {
         lock (_locker)
         {
-            while (!(_state == 2 && _hydrogenwasprinted))
+
+            while (!(_switch == 1 && _number_of_O_printed < 1))
             {
-                Console.WriteLine($"    {Thread.CurrentThread.Name} acquiring lock");
                 Monitor.Wait(_locker);
-                if (_oxygenwasprinted)
+                if (_number_of_H_printed < 2 && _number_of_O_printed ==  1)
                 {
-                    Console.WriteLine($"        {Thread.CurrentThread.Name} pulsing one before reacquiring lock");
                     Monitor.Pulse(_locker);
                 }
             }
-            _state = 1;
+            _switch = 2;
         }
 
         releaseOxygen();
 
         lock (_locker)
         {
-            _hydrogenwasprinted = false;
-            _oxygenwasprinted = true;
-            Console.WriteLine($"        {Thread.CurrentThread.Name} pulsing one at the end");
+            _switch = 1;
+            _number_of_O_printed += 1;
+
+
+
+            if (_number_of_H_printed == 2 && _number_of_O_printed == 1)
+            {
+                _number_of_H_printed = 0;
+                _number_of_O_printed = 0;
+            }
+
             Monitor.Pulse(_locker);
         }
     }
@@ -108,35 +103,47 @@ public class H2O
         }
 
         
-        Thread threadA3 = new Thread(() => foo.Hydrogen(releaseHydrogen));
-        Thread threadA4 = new Thread(() => foo.Hydrogen(releaseHydrogen));
-        Thread threadA2 = new Thread(() => foo.Hydrogen(releaseHydrogen));
-        Thread threadA1 = new Thread(() => foo.Hydrogen(releaseHydrogen));
-        Thread threadB1 = new Thread(() => foo.Oxygen(releaseOxygen));
-        Thread threadB2 = new Thread(() => foo.Oxygen(releaseOxygen));
-        Thread threadB3 = new Thread(() => foo.Oxygen(releaseOxygen));
-        Thread threadB4 = new Thread(() => foo.Oxygen(releaseOxygen));
+        Thread threadH1 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH2 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH3 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH4 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH5 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH6 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH7 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadH8 = new Thread(() => foo.Hydrogen(releaseHydrogen));
+        Thread threadO1 = new Thread(() => foo.Oxygen(releaseOxygen));
+        Thread threadO2 = new Thread(() => foo.Oxygen(releaseOxygen));
+        Thread threadO3 = new Thread(() => foo.Oxygen(releaseOxygen));
+        Thread threadO4 = new Thread(() => foo.Oxygen(releaseOxygen));
 
 
-        threadA1.Name = "threadA1";
-        threadA2.Name = "threadA2";
-        threadA3.Name = "threadA3";
-        threadA4.Name = "threadA4";
-        threadB1.Name = "threadB1";
-        threadB2.Name = "threadB2";
-        threadB3.Name = "threadB3";
-        threadB4.Name = "threadB4";
+        threadH1.Name = "threadH1";
+        threadH2.Name = "threadH2";
+        threadH3.Name = "threadH3";
+        threadH4.Name = "threadH4";
+        threadH5.Name = "threadH5";
+        threadH6.Name = "threadH6";
+        threadH7.Name = "threadH7";
+        threadH8.Name = "threadH8";
+        threadO1.Name = "threadO1";
+        threadO2.Name = "threadO2";
+        threadO3.Name = "threadO3";
+        threadO4.Name = "threadO4";
 
-        threadB4.Start();
 
-        threadA1.Start();
-        threadA2.Start();
-        threadA3.Start();
+        threadH1.Start();
+        threadH2.Start();
+        threadH3.Start();
+        threadH4.Start();
+        threadH5.Start();
+        threadH6.Start();
+        threadH7.Start();
+        threadH8.Start();
 
-        threadB1.Start();
-        threadB2.Start();
-        threadB3.Start();
-        threadA4.Start();
+        threadO2.Start();
+        threadO1.Start();
+        threadO3.Start();
+        threadO4.Start();
 
     }
 }
